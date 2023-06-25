87a3c0985155d40fd38ad039afb4413926995ebe
marta_nowakowicz <nowakow.marta@gmail.com>
2023-06-25 12:11:47 +0200

    Added logging to the preprocessing script

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,9 +1,14 @@
-
-# create SparkSession
 import argparse
+import logging
+
+from typing import Optional
 from pyspark.sql import SparkSession
 from chispa.dataframe_comparer import assert_df_equality
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process csv files.')
@@ -42,20 +47,26 @@ def filter_countries(sdf, countries):
 
 def main():
     spark=SparkSession.builder.appName('data_preprocessing').getOrCreate()
+    logging.info('Spark session created')
+
     # Get arguments from command line
     dataset1_path, dataset2_path, countries = parse_args()
+    logging.info(f"Arguments: dataset1={dataset1_path}, dataset2={dataset2_path}, countries={countries}")
 
     # Read CSV files into Spark DataFrames
     sdf1 = spark.read.csv(dataset1_path, header=True, inferSchema=True)
     sdf2 = spark.read.csv(dataset2_path, header=True, inferSchema=True)
+    logging.info('CSV files read into Spark DataFrames')
 
     sdf_merged = join_dataframes(sdf1,sdf2)
     sdf_merged = drop_columns(sdf_merged, ["first_name", "last_name", "cc_n"])
+
 
     #df_merged_pyspark.select('country').distinct().show()
 
 
     sdf_filtered = filter_countries(sdf_merged, countries)
+    logging.info('Data transformations completed')
     #df_filtered.select('country').distinct().show()
 
     sdf_final = sdf_filtered.selectExpr("id as client_identifier","btc_a as bitcoin_address","cc_t as credit_card_type","country", "email")
@@ -63,6 +74,8 @@ def main():
 
     sdf_final.write.mode('overwrite').option("header",True) \
     .csv("client_data")
+    logging.info('Data written to client_data.csv')
+
 
 if __name__ == '__main__':
     main()
