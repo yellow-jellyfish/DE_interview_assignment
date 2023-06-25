0c032d078726eaa206d6383f720a6eaeaa345a26
marta_nowakowicz <nowakow.marta@gmail.com>
2023-06-25 11:19:45 +0200

    Initial commit with data preprocessing script and requirements file

---
+++ data_preprocessing.py
@@ -0,0 +1,70 @@
+
+# create SparkSession
+import argparse
+from pyspark.sql import SparkSession
+from chispa.dataframe_comparer import assert_df_equality
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process csv files.')
+    parser.add_argument('--dataset1', required=True, help='Path to the first dataset.')
+    parser.add_argument('--dataset2', required=True, help='Path to the second dataset.')
+    parser.add_argument('--countries', required=True, nargs='+', help='Countries to filter.')
+    args = parser.parse_args()
+    return args.dataset1, args.dataset2, args.countries
+
+
+# one branch for testing one for logging 
+# do good commit messages "conventional commit" 
+# do good names 
+# sdf
+# autodocstring
+
+
+def drop_columns(sdf, cols):
+    sdf_dropped = sdf.drop(*cols)
+    #df_dropped.printSchema()
+    return sdf_dropped
+#compare schema 
+
+
+def join_dataframes(sdf1,sdf2):
+    sdf_merged = sdf1.join(sdf2,
+               sdf1.id == sdf2.id,
+               "inner")
+    sdf_merged = sdf_merged.drop(sdf2.id)
+    #sdf_merged.printSchema()
+    return sdf_merged
+
+def filter_countries(sdf, countries):
+    sdf_filtered = sdf.filter(sdf.country.isin(countries))
+    return sdf_filtered
+
+def main():
+    spark=SparkSession.builder.appName('data_preprocessing').getOrCreate()
+    # Get arguments from command line
+    dataset1_path, dataset2_path, countries = parse_args()
+
+    # Read CSV files into Spark DataFrames
+    sdf1 = spark.read.csv(dataset1_path, header=True, inferSchema=True)
+    sdf2 = spark.read.csv(dataset2_path, header=True, inferSchema=True)
+
+    sdf_merged = join_dataframes(sdf1,sdf2)
+    sdf_merged = drop_columns(sdf_merged, ["first_name", "last_name", "cc_n"])
+
+    #df_merged_pyspark.select('country').distinct().show()
+
+
+    sdf_filtered = filter_countries(sdf_merged, countries)
+    #df_filtered.select('country').distinct().show()
+
+    sdf_final = sdf_filtered.selectExpr("id as client_identifier","btc_a as bitcoin_address","cc_t as credit_card_type","country", "email")
+    sdf_final.printSchema()
+
+    sdf_final.write.mode('overwrite').option("header",True) \
+    .csv("client_data")
+
+if __name__ == '__main__':
+    main()
+
+
